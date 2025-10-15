b3a05893ece9291c4fb5239c1e735d52b6fdefaf
Igor <80823011+igorwastaken@users.noreply.github.com>
2025-10-15 13:39:31 +0000

    1.0.0
    Hello, i'm just testing github ci

--- packages/snipp/tests/main.test.ts
+++ packages/snipp/tests/main.test.ts
@@ -1,7 +1,7 @@
 import { Client, User } from "../src";
 
 describe("Trying to create client", () => {
-  const client = new Client(process.env["API-KEY"] as string); // fake key
+  const client = new Client(process.env["API_KEY"] as string); // fake key
   test("Get user uploads", async () => {
     const result = await client.getGallery();
     console.info(result.uploads[0]);
